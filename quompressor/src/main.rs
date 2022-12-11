c85bd6a3d585e3a4c254ab1c3c0a3f3de13ba8de
gabrielmougard.com <gabriel.mougard@gmail.com>
2022-12-11 12:27:07 +0100

    [QUOMPRESSOR] base cli struct

--- quompressor/src/main.rs
+++ quompressor/src/main.rs
@@ -1,10 +1,24 @@
-
+fn exit(msg: &str, code: i32) -> ! {
+    eprintln!("{}", msg);
+    std::process::exit(code);
+}
 
 fn main() {
     let cli_matches = clap::App::new("quompressor")
         .version("0.1.0")
         .author("gabrielmougard")
-        .about("Converts to and from a quadtree-based image compression format (QIM)")
-        .arg_from_usage("-i, --input 'Convert the input file from PNG or JFIF to QIM'")
+        .about("Converts to and from a QIM image compression format")
+        .arg_from_usage("-i, --into 'Convert the input file from PNG or JFIF to QIM'")
+        .arg_from_usage("-f, --from 'Convert the input file from QIM to PNG'")
+        .arg_from_usage("<INPUT> 'Path to input file`")
+		.arg_from_usage("[OUTPUT] 'Path to output file; defaults to INPUT with a modified file extension`")
         .get_matches();
+
+    let (into, from) = (cli_matches.is_present("into"), cli_matches.is_present("from"));
+    match (into, from) {
+        (true, true) => exit("Only one of -i/--into and -f/--from must be present", 2),
+        (true, false) => {},
+        (false, true) => {},
+        (false, false) => exit("One of -i/--into and -f/--from must be present", 2)
+    }
 }
